35d8a342fd16c3af2655d92e427afc641302790f
liukkui <liukui_work@163.com>
2018-12-10 21:55:13 +0800

    Site updated: 2018-12-10 21:55:12

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
         name: 'Right Now (Na Na Na)',
         artist: 'Am1r',
         url: 'http://m10.music.126.net/20181210221136/5b0aa21bf4270e0a00beeb14a966fe6d/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
-        cover: 'https://www.liukui.tech/images/haha5.gif',
+        cover: 'https://www.liukui.tech/images/haha.gif',
       },
     ]
 });
