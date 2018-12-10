28707205065488701e9c0397ea5b3ac507b247ec
liukkui <liukui_work@163.com>
2018-12-10 21:41:06 +0800

    Site updated: 2018-12-10 21:41:06

---
+++ dist/music.js
@@ -0,0 +1,44 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      
+      {
+        name: '空',
+        artist: '暗杠',
+        url: 'http://m10.music.126.net/20181210220448/41adc0c770542fe8e84e30ea32905de5/ymusic/4614/ef38/ccfb/26965dc060c6d3f9bc5c6bae01c70516.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: 'トリカゴ',
+        artist: 'Kyle Xian',
+        url: 'http://m10.music.126.net/20181210212437/ddeb96db3724654925000be95275fd22/ymusic/9d9b/4a9f/b582/4eee6309d187e1ac738d1b939430edbe.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: '光るなら',
+        artist: 'Goose house',
+        url: 'http://m10.music.126.net/20181210212747/723c0313dd963b37febf05230e5e7a18/ymusic/9e0a/6075/ec14/a0338148b6233f2b8a48286db4125b50.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: 'Runaway',
+        artist: 'Sasha Sloan',
+        url: 'http://m10.music.126.net/20181210212943/03c3c814e0dd47ae255913a0d09e124e/ymusic/ced7/509e/e4b2/10b78b6cc7992b31d47d8ae4b64c710c.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: 'Explore',
+        artist: 'Prince of Spain',
+        url: 'http://m10.music.126.net/20181210213038/489f55085f700bc82d6bbe6a32cf51a3/ymusic/39bb/cd5f/61f5/2b918d7156546900a2434384dce9c581.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: 'Right Now (Na Na Na)',
+        artist: 'Am1r',
+        url: 'http://m10.music.126.net/20181210213135/aa743f85bd69402a16210257ede8954a/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+    ]
+});
