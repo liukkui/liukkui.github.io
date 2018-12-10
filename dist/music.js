264910b16f422348baaa43b76346c4bf5539e0e0
liukkui <liukui_work@163.com>
2018-12-10 21:59:11 +0800

    Site updated: 2018-12-10 21:59:10

--- dist/music.js
+++ dist/music.js
@@ -9,5 +9,11 @@ const ap = new APlayer({
         url: 'http://m10.music.126.net/20181210221136/5b0aa21bf4270e0a00beeb14a966fe6d/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
+      {
+        name: 'トリカゴ',
+        artist: 'Kyle Xian',
+        url: 'http://m10.music.126.net/20181210220952/eeea2e4ac54c74d779e0251f417d8727/ymusic/4fc3/a588/ceab/44264869e46520cd90e29b1b61ffee10.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
     ]
 });
