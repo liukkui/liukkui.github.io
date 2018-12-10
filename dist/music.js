2362466c50ea7067a0c759f44dde9d24f7da2560
liukkui <liukui_work@163.com>
2018-12-10 21:47:17 +0800

    Site updated: 2018-12-10 21:47:16

--- dist/music.js
+++ dist/music.js
@@ -3,7 +3,6 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-      
       {
         name: '空',
         artist: '暗杠',
@@ -13,31 +12,31 @@ const ap = new APlayer({
       {
         name: 'トリカゴ',
         artist: 'Kyle Xian',
-        url: 'http://m10.music.126.net/20181210212437/ddeb96db3724654925000be95275fd22/ymusic/9d9b/4a9f/b582/4eee6309d187e1ac738d1b939430edbe.mp3',
+        url: 'http://m10.music.126.net/20181210220952/eeea2e4ac54c74d779e0251f417d8727/ymusic/4fc3/a588/ceab/44264869e46520cd90e29b1b61ffee10.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
         name: '光るなら',
         artist: 'Goose house',
-        url: 'http://m10.music.126.net/20181210212747/723c0313dd963b37febf05230e5e7a18/ymusic/9e0a/6075/ec14/a0338148b6233f2b8a48286db4125b50.mp3',
+        url: 'http://m10.music.126.net/20181210221020/b7651c34e579fb9d5dbb9daa2c6afe53/ymusic/9e0a/6075/ec14/a0338148b6233f2b8a48286db4125b50.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
         name: 'Runaway',
         artist: 'Sasha Sloan',
-        url: 'http://m10.music.126.net/20181210212943/03c3c814e0dd47ae255913a0d09e124e/ymusic/ced7/509e/e4b2/10b78b6cc7992b31d47d8ae4b64c710c.mp3',
+        url: 'http://m10.music.126.net/20181210221057/aee3c82551648a63555ab8dbcdcfca4b/ymusic/ced7/509e/e4b2/10b78b6cc7992b31d47d8ae4b64c710c.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
         name: 'Explore',
         artist: 'Prince of Spain',
-        url: 'http://m10.music.126.net/20181210213038/489f55085f700bc82d6bbe6a32cf51a3/ymusic/39bb/cd5f/61f5/2b918d7156546900a2434384dce9c581.mp3',
+        url: 'http://m10.music.126.net/20181210221115/1b6dc8810c8a29992754e4b75c120241/ymusic/39bb/cd5f/61f5/2b918d7156546900a2434384dce9c581.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
         name: 'Right Now (Na Na Na)',
         artist: 'Am1r',
-        url: 'http://m10.music.126.net/20181210213135/aa743f85bd69402a16210257ede8954a/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
+        url: 'http://m10.music.126.net/20181210221136/5b0aa21bf4270e0a00beeb14a966fe6d/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
     ]
