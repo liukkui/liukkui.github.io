f576960bc7f710c670ef96a667bc23b6c9beb007
liukkui <liukui_work@163.com>
2018-12-11 08:54:46 +0800

    Site updated: 2018-12-11 08:54:46

--- dist/music.js
+++ dist/music.js
@@ -4,15 +4,15 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: 'Right Now (Na Na Na)',
-        artist: 'Am1r',
-        url: 'http://m10.music.126.net/20181211085736/9033555b3e9e13e19ca99f0b09c9a8dc/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
-        cover: 'https://www.liukui.tech/images/haha.gif',
-      },
-      {
         name: 'トリカゴ',
         artist: 'Kyle Xian',
         url: 'http://other.web.nf01.sycdn.kuwo.cn/resource/n1/57/8/3372842215.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: 'Right Now (Na Na Na)',
+        artist: 'Am1r',
+        url: 'http://sg.sycdn.kuwo.cn/resource/n1/91/17/2649203588.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
