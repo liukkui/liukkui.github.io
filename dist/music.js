3b0ca89f8719ba7e1f16fb0a1e4d386bbb0c1b41
liukkui <liukui_work@163.com>
2018-12-11 08:39:17 +0800

    Site updated: 2018-12-11 08:39:16

--- dist/music.js
+++ dist/music.js
@@ -6,19 +6,19 @@ const ap = new APlayer({
       {
         name: 'Right Now (Na Na Na)',
         artist: 'Am1r',
-        url: 'http://m10.music.126.net/20181210221136/5b0aa21bf4270e0a00beeb14a966fe6d/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
+        url: 'http://m10.music.126.net/20181211085736/9033555b3e9e13e19ca99f0b09c9a8dc/ymusic/e25b/d5fe/d2de/e8e3e8abe9c8dafa66e6e21d9a9c5ba0.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
         name: 'トリカゴ',
         artist: 'Kyle Xian',
-        url: 'http://m10.music.126.net/20181210220952/eeea2e4ac54c74d779e0251f417d8727/ymusic/4fc3/a588/ceab/44264869e46520cd90e29b1b61ffee10.mp3',
+        url: 'http://other.web.nf01.sycdn.kuwo.cn/resource/n1/57/8/3372842215.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
         name: '光るなら',
         artist: 'Goose house',
-        url: 'http://m10.music.126.net/20181210221020/b7651c34e579fb9d5dbb9daa2c6afe53/ymusic/9e0a/6075/ec14/a0338148b6233f2b8a48286db4125b50.mp3',
+        url: 'http://other.web.rf01.sycdn.kuwo.cn/resource/n1/77/17/2299001074.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
     ]
