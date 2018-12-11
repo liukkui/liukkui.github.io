db9325737371d0936739022ad9c4c57fe0c14ae3
liukkui <liukui_work@163.com>
2018-12-11 08:56:37 +0800

    Site updated: 2018-12-11 08:56:36

--- dist/music.js
+++ dist/music.js
@@ -4,6 +4,12 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
+        name: '光るなら',
+        artist: 'Goose house',
+        url: 'http://other.web.rf01.sycdn.kuwo.cn/resource/n1/77/17/2299001074.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
         name: 'トリカゴ',
         artist: 'Kyle Xian',
         url: 'http://other.web.nf01.sycdn.kuwo.cn/resource/n1/57/8/3372842215.mp3',
@@ -13,12 +19,6 @@ const ap = new APlayer({
         name: 'Right Now (Na Na Na)',
         artist: 'Am1r',
         url: 'http://sg.sycdn.kuwo.cn/resource/n1/91/17/2649203588.mp3',
-        cover: 'https://www.liukui.tech/images/haha.gif',
-      },
-      {
-        name: '光るなら',
-        artist: 'Goose house',
-        url: 'http://other.web.rf01.sycdn.kuwo.cn/resource/n1/77/17/2299001074.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
     ]
