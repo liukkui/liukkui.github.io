55a9c2a16aa207a7e6a0bf2dfdb94f2bd8bedfee
liukkui <liukui_work@163.com>
2018-12-12 20:58:13 +0800

    Site updated: 2018-12-12 20:58:12

--- dist/music.js
+++ dist/music.js
@@ -4,6 +4,24 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
+        name: '走歌人',
+        artist: '杠姐',
+        url: 'http://other.web.ri01.sycdn.kuwo.cn/resource/n3/1/70/780373770.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: 'Explore',
+        artist: 'Prince of Spain',
+        url: 'http://other.web.ri01.sycdn.kuwo.cn/resource/n2/83/10/1648760572.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: '1965',
+        artist: 'Zella Day',
+        url: 'http://other.web.ri01.sycdn.kuwo.cn/resource/n1/95/42/3589516223.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
         name: '光るなら',
         artist: 'Goose house',
         url: 'http://other.web.rf01.sycdn.kuwo.cn/resource/n1/77/17/2299001074.mp3',
@@ -16,15 +34,15 @@ const ap = new APlayer({
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
-        name: 'Right Now (Na Na Na)',
-        artist: 'Am1r',
-        url: 'http://sg.sycdn.kuwo.cn/resource/n1/91/17/2649203588.mp3',
+        name: 'Beautiful Now',
+        artist: 'Zedd',
+        url: 'http://other.web.nc01.sycdn.kuwo.cn/resource/n3/30/0/3496398090.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
-        name: 'Explore',
-        artist: 'Prince of Spain',
-        url: 'http://other.web.ri01.sycdn.kuwo.cn/resource/n2/83/10/1648760572.mp3',
+        name: 'Right Now (Na Na Na)',
+        artist: 'Am1r',
+        url: 'http://sg.sycdn.kuwo.cn/resource/n1/91/17/2649203588.mp3',
         cover: 'https://www.liukui.tech/images/haha.gif',
       },
       {
