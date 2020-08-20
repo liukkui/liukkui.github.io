6d7d273259703c80b9ddbbcb2a522c324f797b90
liukkui <liukui_work@163.com>
2020-08-20 23:13:31 +0800

    Site updated: 2020-08-20 23:13:28

---
+++ dist/music.js
@@ -0,0 +1,50 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+       {
+        name: 'Jerome',
+        artist: 'Zella Day',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_6364210&response=res&type=convert_url&',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: '小小恋歌',
+        artist: '新垣結衣',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_1002761&response=res&type=convert_url&',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+       {
+        name: 'Explore',
+        artist: 'Prince of Spain',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_3573061&response=res&type=convert_url&',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: '走歌人',
+        artist: '杠姐',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_27825128&response=res&type=convert_url&',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: '1965',
+        artist: 'Zella Day',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_6364213&response=res&type=convert_url&',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: '光るなら',
+        artist: 'Goose house',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_6400506&response=res&type=convert_url&',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+      {
+        name: 'トリカゴ',
+        artist: 'Kyle Xian',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_41186764&response=res&type=convert_url&',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+    ]
+});
+
