ec6b1ca93d3483da4a6475a8cb91d57e5618b610
liukkui <liukui_work@163.com>
2020-08-26 19:28:04 +0800

    Site updated: 2020-08-26 19:28:02

--- dist/music.js
+++ dist/music.js
@@ -4,6 +4,12 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
     {
+        name: "Explore",
+        artist: 'Prince of Spain',
+        url: 'http://music.163.com/song/media/outer/url?id=27810132.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+    {
         name: "1965",
         artist: 'Zella Day',
         url: 'https://sharefs.yun.kugou.com/202008261143/c97e484cf20b88d1aab47a5ef220557a/G111/M06/0C/1B/D4cBAFvm_jaAM-KdADU1t9Z612M078.mp3',
