c11194b4292eac63e02861923826139d142c03dc
liukkui <liukui_work@163.com>
2020-08-26 11:46:25 +0800

    Site updated: 2020-08-26 11:46:23

--- dist/music.js
+++ dist/music.js
@@ -3,6 +3,12 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
+    {
+        name: "1965",
+        artist: 'Zella Day',
+        url: 'https://sharefs.yun.kugou.com/202008261143/c97e484cf20b88d1aab47a5ef220557a/G111/M06/0C/1B/D4cBAFvm_jaAM-KdADU1t9Z612M078.mp3',
+        cover: 'https://www.liukui.tech/haha.gif',
+      }
     ]
 });
 
