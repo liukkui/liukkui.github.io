c45c19e5ca69d9e7fd5be5afbd338ce19246bdbb
liukkui <liukui_work@163.com>
2020-10-01 17:16:42 +0800

    Site updated: 2020-10-01 17:16:37

--- dist/music.js
+++ dist/music.js
@@ -14,6 +14,12 @@ const ap = new APlayer({
         lrc: 'https://www.liukui.tech/images/Explore-Prince of Spain.lrc',
       },
     {
+        name: "Let Me Down Slowly",
+        artist: 'Alec Benjamin',
+        url: 'https://sharefs.yun.kugou.com/202008312109/8ecc6e4cefb2e1b772fbb4efee3ff2bd/G223/M00/0B/11/f4cBAF7hkcSAEcNXACgnW9Lo4fQ178.mp3',
+        cover: 'https://www.liukui.tech/images/haha.gif',
+      },
+    {
         name: "1965",
         artist: 'Zella Day',
         url: 'http://music.163.com/song/media/outer/url?id=28798452.mp3',
